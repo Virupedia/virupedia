6c62da30946fa46ff0371e08b013e095070c9f80
Ahmed <afakhfekh@gmail.com>
2020-11-27 10:02:48 +0100

    frontcommmit

--- views/front/header.php
+++ views/front/header.php
@@ -25,7 +25,7 @@
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
      <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
-
+    
      <link rel="stylesheet" href="css/bootstrap.min.css">
      <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
      <link rel="stylesheet" href="css/magnific-popup.css">
--- views/front/index.php
+++ views/front/index.php
@@ -11,7 +11,7 @@
   <title>Virupedia</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!--<link rel="shortcut icon" type="image/x-icon" href="images/logo.png"> logo change error-->
+ <link rel="shortcut icon" type="image/x-icon" href="images/logo.png"> 
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="fonts/icomoon/style.css">
 
