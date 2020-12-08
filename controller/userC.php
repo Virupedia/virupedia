71df660894da8424e154e60667f5ba677dc6c271
fatma fendri <fatma.fendri@esprit.tn>
2020-12-08 18:02:46 +0100

    userC

--- controller/userC.php
+++ controller/userC.php
@@ -1,6 +1,6 @@
 <?PHP
-include "C://xampp/htdocs/webprojettest/allfolders/config.php";
-
+include "C://xampp/htdocs/Virupedia/virupedia/config.php";
+require_once 'C://xampp/htdocs/Virupedia/virupedia/model/users.php';
 class UtilisateurC
 {
 
@@ -38,4 +38,61 @@ class UtilisateurC
             die('Erreur: ' . $e->getMessage());
         }
     }
+
+    function supprimerUtilisateurs($id)
+    {
+        $sql = "DELETE FROM users WHERE idUsers = :id";
+        $db = config::getConnexion();
+        $req = $db->prepare($sql);
+        $req->bindValue(':id', $id);
+        try {
+            $req->execute();
+        } catch (Exception $e) {
+            die('Erreur: ' . $e->getMessage());
+        }
+    }
+    function recupererUtilisateur($id){
+        $sql="SELECT * from users where idUsers =$id";
+        $db = config::getConnexion();
+        try{
+            $query=$db->prepare($sql);
+            $query->execute();
+
+            $user=$query->fetch();
+            return $user;
+        }
+        catch (Exception $e){
+            die('Erreur: '.$e->getMessage());
+        }
+    }
+
+    function modifierUtilisateur($Utilisateur, $id)
+    {
+        try {
+            $db = config::getConnexion();
+            $query = $db->prepare(
+                'UPDATE Users SET 
+                     nameUsers= :nameUsers, 
+                     lastnameUsers= :lastnameUsers,
+                     address= :address,
+                     Login= :Login,
+                     Cin= :Cin,
+                     Password= :Password
+                WHERE idUsers = :idUsers'
+            );
+            $query->execute([
+                'nameUsers' => $Utilisateur->getNom(),
+                'lastnameUsers' => $Utilisateur->getPrenom(),
+                'address' => $Utilisateur->getAdress(),
+                'Login' => $Utilisateur->getLogin(),
+                'Cin' => $Utilisateur->getCin(),
+                'Password' => $Utilisateur->getPassword(),
+                'idUsers' =>$id
+            ]);
+            echo $query->rowCount() . " records UPDATED successfully <br>";
+        } catch (PDOException $e) {
+            $e->getMessage();
+        }
+    }
+
 }
